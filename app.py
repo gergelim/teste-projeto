150139ab9f4289853b8d13d6cf302a556a551b95
gergelim <avkamargo@gmail.com>
2025-11-05 11:52:18 -0300

    update app.py

--- app.py
+++ app.py
@@ -4,15 +4,10 @@ import uvicorn
 app = FastAPI()
 
 @app.get("/")
-<<<<<<< HEAD
-=======
-
->>>>>>> 200f8b94f31d6b87fe4d6eabe9f7cce09a6baf5a
 def ola_mundo():
     return{"teste":"Olá mundo!"}
 
 @app.get("/teste")
-<<<<<<< HEAD
 def teste():
     return{"teste":"testando"}
 
@@ -22,11 +17,3 @@ def alunos():
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
-=======
-
-def teste():
-    return{"teste":"testando"}
-
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
->>>>>>> 200f8b94f31d6b87fe4d6eabe9f7cce09a6baf5a
