d93b6245351a03abecb6b830f303c636b0cb1b69
gergelim <avkamargo@gmail.com>
2025-11-05 11:50:26 -0300

    update alunos

---
+++ app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def ola_mundo():
+    return{"teste":"Olá mundo!"}
+
+@app.get("/teste")
+def teste():
+    return{"teste":"testando"}
+
+@app.get("/alunos")
+def alunos():
+    return{"nome":"andré","turma":"imi3"}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
